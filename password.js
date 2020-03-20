e5fd6374835495c56c7a8bc5a05bdc91e8a724bb
unknown <rashadbayar@gmail.com>
2020-03-19 17:56:29 -0700

    update

--- password.js
+++ password.js
@@ -1,12 +1,20 @@
 
 var x = "0123456789!@#$%^&**()_-+[]:'.?ABCDEFGHIJKLMNOPKQRSTUVWXYZabcdefghijklmnopkqrstuvxwyz";
 
+var askQuestion0 = confirm("do you want a random password?");
+var askQuestion1 = confirm("A password that include a uppercase?");
+var askQuestion2 = confirm("A password that Include a number?");
+var askQuestion3 = confirm("A password that include a lowerase?");
+var askQuestion4= confirm("A password that Include a special character?");
+var askQuestion5 = confirm("click on generate password");
+
+
 function generate() {
     var password='';
     for (var index = 1; index <= 10; index++) {
         var c = Math.floor((Math.random()*x.length)+0);
         password+= x.charAt(c);
     }
-    console.log(password);
+    alert(password);
     document.getElementById('pass').innerHTML=`<h1>Password: ${password}</h1>`; 
 }
