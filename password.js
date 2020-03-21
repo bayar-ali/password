bd63cadbe46b4a693e317e124aeea0d2ba6acafb
unknown <rashadbayar@gmail.com>
2020-03-20 20:51:45 -0700

    update

--- password.js
+++ password.js
@@ -1,5 +1,5 @@
 
-var x = "0123456789!@#$%^&**()_-+[]:'.?ABCDEFGHIJKLMNOPKQRSTUVWXYZabcdefghijklmnopkqrstuvxwyz";
+//questions
 
 var askQuestion0 = confirm("do you want a random password?");
 var askQuestion1 = confirm("A password that include a uppercase?");
@@ -8,12 +8,15 @@ var askQuestion3 = confirm("A password that include a lowerase?");
 var askQuestion4= confirm("A password that Include a special character?");
 var askQuestion5 = confirm("click on generate password");
 
+// my variable 
+var generatePassword = "0123456789!@#$%^&**()_-+[]:'.?ABCDEFGHIJKLMNOPKQRSTUVWXYZabcdefghijklmnopkqrstuvxwyz";
 
+// my function
 function generate() {
     var password='';
     for (var index = 1; index <= 10; index++) {
-        var c = Math.floor((Math.random()*x.length)+0);
-        password+= x.charAt(c);
+        var c = Math.floor((Math.random()*generatePassword.length)+0);
+        password+= generatePassword.charAt(c);
     }
     alert(password);
     document.getElementById('pass').innerHTML=`<h1>Password: ${password}</h1>`; 
